f9fb149a98ba094819728284d612fc56f2d088b6
MaianeLuiza <maianepaixao025@gmail.com>
2023-02-28 23:17:30 -0300

    Primeiras modificações no script

---
+++ script.js
@@ -0,0 +1,47 @@
+let musica = document.querySelector('audio');
+
+let duracaoMusica = document.querySelector('#fim');
+let imagem = document.querySelector('img');
+let nomeDaMusica = document.querySelector('.descricao h2');
+let nomeDoArtista = document.querySelector('.descricao i');
+
+duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+
+// Eventos 
+
+document.querySelector('.iconPlay').addEventListener('click', tocarMusica);
+
+document.querySelector('.iconPause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+// Funções
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.iconPause').style.display = 'block';
+    document.querySelector('.iconPlay').style.display = 'none';
+}
+
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.iconPause').style.display = 'none';
+    document.querySelector('.iconPlay').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('#inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
+}
